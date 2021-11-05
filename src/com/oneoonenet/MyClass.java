d402a17fd21d5f95802e2bef644cead1915731fc
meeuwsstefaan <stefaanmeeuws@gmail.com>
2021-11-05 23:53:46 +0100

    Split into 2 files: my own solution vs. udemy blog solution.

--- src/com/oneoonenet/MyClass.java
+++ src/com/oneoonenet/MyClass.java
@@ -2,16 +2,7 @@ package com.oneoonenet;
 
 import java.util.Scanner;
 
-// public class Main {
-//    public static void main(String[] args) {
-//        int n = 10;
-//        int result = 1;
-//
-//        for (int i = 1; i < n; i++) {
-//            result += result * i;
-//        }
-//        System.out.println(n + "! = " + result);
-//    }
+
 
 public class MyClass {
     public static void main(String[] args) {
