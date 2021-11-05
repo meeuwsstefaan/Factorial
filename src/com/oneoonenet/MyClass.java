4356a3affbd37081a1b4ebd4ce596929c20b8184
meeuwsstefaan <stefaanmeeuws@gmail.com>
2021-11-05 23:44:29 +0100

    Code refactoring: common part of if in factorial() amalgamated.

--- src/com/oneoonenet/MyClass.java
+++ src/com/oneoonenet/MyClass.java
@@ -21,6 +21,7 @@ public class MyClass {
         int factorialNumber = userInput.nextInt();
         System.out.println("The factorial of " + factorialNumber + " is " + text.factorial(factorialNumber));
     }
+
     public int factorial(int f) {
         if (f != 1) {
             f = f * factorial(f - 1);
